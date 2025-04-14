fd46a092bc7e1a279fe05df8bd5e08c8d0a4a854
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-04-14 09:31:48 -0600

    Added drawBoot method code

--- app/src/main/java/drawingproject/view/CanvasPanel.java
+++ app/src/main/java/drawingproject/view/CanvasPanel.java
@@ -23,7 +23,18 @@ public class CanvasPanel extends JPanel {
    }
 
    private Polygon drawBoot(){
-      Polygon shape = new Polygon();
+      Polygon shape;
+
+      double scaleFactor = 1;
+      int[] xPoints = {0, 30, 40, 60, 20, 0};
+      int[] yPoints = {0, 0, 30, 50, 40, 0};
+
+      for (int index = 0; index < xPoints.length; index++){
+         xPoints[index] = (int) Math.round(xPoints[index] * scaleFactor);
+         yPoints[index] = (int) Math.round(yPoints[index] * scaleFactor);
+      }
+
+      shape = new Polygon(xPoints, yPoints, xPoints.length);
 
       return shape;
    }
